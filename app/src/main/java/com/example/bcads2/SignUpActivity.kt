81b581a2644db37c74e7c692cdc31b7621ca27e9
dibaliqaja <dibaliqaja@gmail.com>
2021-03-25 11:20:11 +0700

    add action in button

--- app/src/main/java/com/example/bcads2/SignUpActivity.kt
+++ app/src/main/java/com/example/bcads2/SignUpActivity.kt
@@ -1,11 +1,23 @@
 package com.example.bcads2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        btn_register.setOnClickListener {
+            finish()
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+
+        tv_signin.setOnClickListener {
+            finish()
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/bcads2/WelcomeActivity.kt
+++ app/src/main/java/com/example/bcads2/WelcomeActivity.kt
@@ -1,11 +1,18 @@
 package com.example.bcads2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        btn_started.setOnClickListener {
+            finish()
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
     }
 }

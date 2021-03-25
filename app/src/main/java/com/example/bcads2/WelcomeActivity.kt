f5e586451b7d2d4a2282392c7bf6d01068434719
dibaliqaja <dibaliqaja@gmail.com>
2021-03-25 10:45:37 +0700

    add design home

--- app/src/main/java/com/example/bcads2/WelcomeActivity.kt
+++ app/src/main/java/com/example/bcads2/WelcomeActivity.kt
@@ -3,9 +3,9 @@ package com.example.bcads2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcome)
     }
 }
